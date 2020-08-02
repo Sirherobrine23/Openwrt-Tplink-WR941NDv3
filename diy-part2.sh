2075942b60abc55a2476aa39d519cf7326788830
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-08-01 22:07:35 -0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-#============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#============================================================
-
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 #Hostaname
@@ -18,7 +9,9 @@ sed -i 's/1.openwrt.pool.ntp.org/b.st1.ntp.br/g' package/base-files/files/bin/co
 sed -i 's/2.openwrt.pool.ntp.org/c.st1.ntp.br/g' package/base-files/files/bin/config_generate
 sed -i 's/3.openwrt.pool.ntp.org/d.st1.ntp.br/g' package/base-files/files/bin/config_generate
 #UTC/TimeZone
-sed -i 's/UTC/<-03>3/g' package/base-files/files/bin/config_generate
+sed -i 's/UTC/UTC-3/g' package/base-files/files/bin/config_generate
 #OpenWRT Wireless Activated
 sed -i 's/set wireless.radio${devidx}.disabled=1/set wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_radio${devidx}.ssid=Openwrt-WR941NDv3/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# meus pacotes (BETA)
+wget https://script.sirherobrine23.org/openwrt-internet-status.sh -O package/base-files/files/sbin/status;chmod +x package/base-files/files/sbin/status;chmod 775 package/base-files/files/sbin/status
