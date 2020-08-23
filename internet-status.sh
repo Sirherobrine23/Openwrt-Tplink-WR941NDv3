230ba6e6784748b740cbeb793ad72c7ad2b7fd2c
Matheus Sampaio Queiroga <matheus@iMac-de-Matheus.lan>
2020-08-22 23:35:40 -0300

    update 2

---
+++ internet-status.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+LED="/sys/class/leds/tp-link:green:qss/brightness"
+URL="http://google.com.br"
+update_led="2s"
+# Iniciando o loop
+while [ true ]; do 
+wget -q -O tested --spider "$URL"
+if [ $? -eq 0 ]; then
+  echo "1" > $LED
+else
+  echo "0" > $LED
+fi
+sleep $update_led
+done
