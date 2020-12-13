94a23b3c19d47d1f325171b52e569efb466ffd51
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-12-13 00:43:03 -0300

    push

--- internet-status.sh
+++ internet-status.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 LED="/sys/class/leds/tp-link:green:qss/brightness"
-URL="http://apt.sh23.org/"
-update_led="2s"
-# Iniciando o loop
+URL="http://ftp.debian.org"
+update_led="1s"
+# Starting the loop
 while [ true ]; do 
-wget -q -O tested --spider "$URL"
-if [ $? -eq 0 ]; then
+if wget -q -O /tmp/tested --spider "$URL"; then
   echo "1" > $LED
 else
   echo "0" > $LED
