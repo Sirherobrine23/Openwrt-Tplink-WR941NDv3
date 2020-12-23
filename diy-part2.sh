0bfe9a7e51ec13d434c9aa0ead0b9f89354ba5b3
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-12-23 17:09:25 -0300

    push

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 #Hostaname
 sed -i 's/OpenWrt/r1/g' package/base-files/files/bin/config_generate
 #NTP Server Brazil
@@ -14,8 +14,15 @@ sed -i 's/UTC/UTC-3/g' package/base-files/files/bin/config_generate
 sed -i 's/set wireless.radio${devidx}.disabled=1/set wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_radio${devidx}.ssid=Openwrt-WR941NDv3/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # meus pacotes (BETA)
-cp ../internet-status.sh package/base-files/files/bin/status;chmod +x package/base-files/files/bin/status;chmod 775 package/base-files/files/bin/status
-cp ../system-autoupdate.sh package/base-files/files/bin/system-autoupdate;chmod +x package/base-files/files/bin/system-autoupdate;chmod 775 package/base-files/files/bin/system-autoupdate
+# Internet Status
+cp ../internet-status.sh package/base-files/files/bin/status
+chmod +x package/base-files/files/bin/status
+chmod 775 package/base-files/files/bin/status
+
+# Auto Update
+cp ../system-autoupdate.sh package/base-files/files/bin/system-autoupdate
+chmod +x package/base-files/files/bin/system-autoupdate
+chmod 775 package/base-files/files/bin/system-autoupdate
 # init status
 echo "status &" > package/base-files/files/etc/rc.local
 echo 'exit 0' >> package/base-files/files/etc/rc.local
